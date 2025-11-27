ddbc5d82c4538ca3cdf544bdf7ca145a9205972f
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2025-11-27 18:33:50 +0500

    Add files via upload

---
+++ Operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First Number: ";
+    cin>> a;
+    cout<<"Enter Second Number: ";
+    cin>> b;
+
+    cout << "a+b= "<< a+b <<endl;
+    cout << "a-b= "<< a-b <<endl;
+    cout << "a*b= "<< a*b <<endl;
+    cout << "a/b= "<< (float)a/b <<endl;
+}
