38ed7245cecd8bd78e874f91c2a02bb2eba282ad
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2025-12-01 01:16:45 +0500

    Add files via upload

---
+++ Strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name="Ahmed Ali";
+    cout<< "Name is: "<< name<<endl;
+    cout<<"Lenght is: "<<name.length()<< endl;
+    cout <<"Indexing: "<< name.substr(1,4)<< endl;
+
+}
